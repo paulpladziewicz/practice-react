dcbac3e46a7a0f3b3a9b72a70c19239b1c9e3f7d
paulpladziewicz <ppladziewicz@gmail.com>
2022-07-18 15:05:15 -0400

    added project boilerplate

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,18 @@
 // use const for functions so that functions are not susceptible to change.
 export const Todo = (props) => {
-    return (
-        <li>{ props.item } <button>Edit</button><button>Update</button><button>Delete</button> </li>
-    )
+    // practice destructuring props
 
+    const handleOnChange = () => {
+        console.log('checked');
+    }
+
+    return (
+        <li>
+            { props.item }
+            <input type='checkbox' checked={false} onChange={handleOnChange}/>
+            <button>Edit</button>
+            <button>Delete</button>
+        </li>
+    );
  };
 
