b94e280e2be31318dfa1c0c463dbaafe85563952
Kayode Oseni <52648319+kosenijr@users.noreply.github.com>
2023-01-03 19:20:23 -0500

    set up complete js and complementary module css for ensuring the organization of finished tasks

---
+++ src/components/Complete.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import styles from '../styles/Complete.module.css'
+import Todo from './Todo'
+import List from './TodoList'
+
+
+
+const Complete = () => {
+    return (
+        <div className={styles['complete-div']}>
+            <h2>Complete!</h2>
+        </div>
+    )
+}
+
+export default Complete
